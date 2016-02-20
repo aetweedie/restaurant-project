84919005bc138587ffaa0411c7711a033d661520
Michael DiGuiseppi <michael.diguiseppi@gmail.com>
2016-02-19 23:30:47 -0700

    created index page layout, working on form for new restaurant page

--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -2,7 +2,16 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', {
+    restaurants: [ {name: 'McDonalds', image: 'burger.png'}, {name: 'Pizza Hut',
+    image: 'italian.png'}, {name: 'Taco Bell', image: 'mexican.png'}, {name: 'Pho99',
+    image: 'pho.jpg'}, {name: 'Thai Kitchen', image: 'thai.jpg'}, {name: 'Maggianos',
+    image: 'italian.png'}]
+   });
+});
+
+router.get('/new', function(req, res, next) {
+    res.render('new');
 });
 
 module.exports = router;
