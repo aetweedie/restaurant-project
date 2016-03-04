31aec0576977af7f845bc811a4131be58529c3b7
Michael DiGuiseppi <michael.diguiseppi@gmail.com>
2016-03-04 15:34:15 -0700

    Added queries files and starting to refactor with knex

--- src/server/routes/apiRoutes.js
+++ src/server/routes/apiRoutes.js
@@ -2,6 +2,8 @@ var express     = require('express');
 var restaurant  = require('./utility');
 var router      = express.Router();
 var pg          = require('pg');
+var res_queries = require('../restaurant_queries');
+var rev_queries = require('../review_queries');
 
 // connect to env.DATABASE_URL or localhost
 var connectionString = process.env.DATABASE_URL || 'postgres://localhost:5432/gtables';
@@ -19,7 +21,7 @@ router.get('/restaurants/:id?', function(req, res, next) {
                     message: 'Something bad happened'
                 });
             done();
-        };
+        }
         if (id) {
             var query = client.query('SELECT * FROM restaurants WHERE id = '+id);
         } else {
@@ -48,7 +50,7 @@ router.post('/restaurants/new', function(req, res, next) {
         if (err) {
             res.status(500).json({status: 'Error', message: 'Couldn\'t retrieve restaurants'});
             done();
-        };
+        }
 
         var query = client.query("INSERT INTO restaurants (name, address_city, address_state, cuisine, image_url, description) VALUES ('"+newRestaurant.name+"', '"+newRestaurant.address_city+"', '"+newRestaurant.address_state+"', '"+newRestaurant.cuisine+"',  '"+newRestaurant.image_url+"', '"+newRestaurant.description+"')");
 
@@ -70,7 +72,7 @@ router.put('/restaurants/:id/edit', function(req, res, next) {
         if (err) {
             res.status(500).json({status: 'Error', message: 'Couldn\'t retrieve restaurants'});
             done();
-        };
+        }
         var query = client.query("UPDATE restaurants SET name = '"+editRestaurant.name+"', address_city = '"+editRestaurant.address_city+"', address_state = '"+editRestaurant.address_state+"', cuisine = '"+editRestaurant.cuisine+"', image_url = '"+editRestaurant.image_url+"', description = '"+editRestaurant.description+"' WHERE id = "+id);
         query.on('end', function() {
             done();
@@ -87,7 +89,7 @@ router.delete('/restaurants/:id/delete', function(req, res, next) {
         if (err) {
             res.status(500).json({status: 'Error', message: 'Couldn\'t retrieve restaurants'});
             done();
-        };
+        }
 
         var query = client.query('DELETE FROM restaurants WHERE id = '+id);
 
@@ -202,7 +204,7 @@ router.put('/restaurants/:id/reviews/:review_id/edit', function(req, res, next) 
         if (err) {
             res.status(500).json({status: 'Error', message: 'Couldn\'t retrieve restaurants'});
             done();
-        };
+        }
         var query = client.query("UPDATE reviews SET reviewer = '"+editReview.reviewer+"', review_date = '"+editReview.review_date+"', rating = "+Number(editReview.rating)+", review = '"+editReview.review+"', restaurant_id = "+id+" WHERE id = "+review_id);
 
         query.on('end', function() {
